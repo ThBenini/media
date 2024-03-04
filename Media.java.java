cbd016fe21ab82f945b706fc459472f5bbbb9bd9
Thiago Benini <139355804+ThBenini@users.noreply.github.com>
2024-03-04 11:28:56 -0300

    Add files via upload

---
+++ Media.java.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Juca {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+
+        double num1,num2,num3,soma;
+
+        System.out.print("Número 01: ");
+        num1 = ler.nextDouble();
+        System.out.print("Número 02: ");
+        num2 = ler.nextDouble();
+        System.out.print("Número 03: ");
+        num3 = ler.nextDouble();
+
+        soma = (num1 + num2 + num3) / 3;
+
+        System.out.print("Resultado: " + soma);
+    }
+
+
+
+
+}
